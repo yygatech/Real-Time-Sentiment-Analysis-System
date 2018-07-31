1b2adc87bb9d6f68535c1fd194a8a76d93bd50d0
yygatech <yygatech@gmail.com>
2018-07-30 19:16:30 -0500

    initial commit

---
+++ src/main/scala/streaming/TwitterProducer.scala
@@ -0,0 +1,103 @@
+package streaming
+
+import java.util.HashMap
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+import twitter4j.Status
+
+/**
+  * A Kafka Producer that gets tweets on certain keywords
+  * from twitter datasource and publishes to a kafka topic
+  *
+  * Arguments:  <KafkaTopic> <keyword_1> ... <keyword_n>
+  * <KafkaTopic>		- The kafka topic to subscribe to
+  * <keyword_1>		- The keyword to filter tweets
+  * <keyword_n>		- Any number of keywords to filter tweets
+  *
+  */
+
+object TwitterProducer {
+  def main(args: Array[String]) {
+    // You can find all functions used to process the stream in the
+    // Utils.scala source file, whose contents we import here
+    import Utils._
+
+      /********
+      * Let's check to make sure user has entered correct parameters
+      *
+      ********/
+    if (args.length < 2) {
+      System.out.println("Usage: TwitterProducer1 <KafkaTopic> <keyword1>")
+      return
+    }
+
+    val topic = args(0).toString
+    val filters = args.slice(1, args.length)
+    val kafkaBrokers = "localhost:9092,localhost:9093"
+
+    // First, let's configure Spark
+    // We have to at least set an application name and master
+    // If no master is given as part of the configuration we
+    // will set it to be a local deployment running an
+    // executor per thread
+    val sparkConfiguration = new SparkConf().
+      setAppName("spark-twitter-stream-example").
+      setMaster(sys.env.get("spark.master").getOrElse("local[*]"))
+
+    // Let's create the Spark Context using the configuration we just created
+    val sparkContext = new SparkContext(sparkConfiguration)
+    sparkContext.setLogLevel("ERROR")
+
+    // Now let's wrap the context in a streaming one, passing along the window size
+    val streamingContext = new StreamingContext(sparkContext, Seconds(5))
+
+
+    val tweets: DStream[Status] =
+    TwitterUtils.createStream(streamingContext, None, filters)
+
+    // Let's extract the words of each tweet
+    // We'll carry the tweet along in order to print it in the end
+    val textAndSentences: DStream[(TweetText, Sentence)] =
+    tweets.filter(x => x.getLang == "en").
+      map(_.getText).
+      map(tweetText => (tweetText, wordsOf(tweetText)))
+
+
+    // write output to screen
+    textAndSentences.print()
+
+    // send data to Kafka broker
+
+    textAndSentences.foreachRDD( rdd => {
+
+      rdd.foreachPartition( partition => {
+        // Print statements in this section are shown in the executor's stdout logs
+        val props = new HashMap[String, Object]()
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokers)
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+          "org.apache.kafka.common.serialization.StringSerializer")
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+          "org.apache.kafka.common.serialization.StringSerializer")
+        val producer = new KafkaProducer[String, String](props)
+        partition.foreach( record => {
+          val data = record.toString
+          val message = new ProducerRecord[String, String](topic, null, data)
+          producer.send(message)
+        } )
+        producer.close()
+      })
+
+    })
+
+    // Now that the streaming is defined, start it
+    streamingContext.start()
+
+    // Let's await the stream to end - forever
+    streamingContext.awaitTermination()
+  }
+
+}
---
+++ src/main/scala/streaming/Utils.scala
@@ -0,0 +1,61 @@
+package streaming
+
+import twitter4j.Status
+
+import scala.io.Source
+
+object Utils {
+
+  // Some type aliases to give a little bit of context
+  type Tweet = Status
+  type TweetText = String
+  type Sentence = Seq[String]
+
+  //private def format(n: Int): String = f"$n%2d"
+
+  private def wrapScore(s: String): String = s"[ $s ] "
+  /*
+    private def makeReadable(n: Int): String =
+      if (n > 0)      s"${AnsiColor.GREEN + format(n) + AnsiColor.RESET}"
+      else if (n < 0) s"${AnsiColor.RED   + format(n) + AnsiColor.RESET}"
+      else            s"${format(n)}"
+  */
+  /*private def makeReadable(s: String): String =
+    s.takeWhile(_ != '\n').take(80) + "..."
+
+  def makeReadable(sn: (String, Int)): String =
+    sn match {
+      case (tweetText, score) => s"${makeReadable(tweetText)}"
+    }*/
+
+  def load(resourcePath: String): Set[String] = {
+    val source = Source.fromInputStream(getClass.getResourceAsStream(resourcePath))
+    val words = source.getLines.toSet
+    source.close()
+    words
+  }
+
+  def wordsOf(tweet: TweetText): Sentence =
+    tweet.split(" ")
+
+  def toLowercase(sentence: Sentence): Sentence =
+    sentence.map(_.toLowerCase)
+
+  def keepActualWords(sentence: Sentence): Sentence =
+    sentence.filter(_.matches("[a-z]+"))
+
+  def extractWords(sentence: Sentence): Sentence =
+    sentence.map(_.toLowerCase).filter(_.matches("[a-z]+"))
+
+  def keepMeaningfulWords(sentence: Sentence, uselessWords: Set[String]): Sentence =
+    sentence.filterNot(word => uselessWords.contains(word))
+
+  def computeScore(words: Sentence, positiveWords: Set[String], negativeWords: Set[String]): Int =
+    words.map(word => computeWordScore(word, positiveWords, negativeWords)).sum
+
+  def computeWordScore(word: String, positiveWords: Set[String], negativeWords: Set[String]): Int =
+    if (positiveWords.contains(word)) 1
+    else if (negativeWords.contains(word)) -1
+    else 0
+
+}
